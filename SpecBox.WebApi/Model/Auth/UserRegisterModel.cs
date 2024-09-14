20dc8b51f2e6e3d2642b9b04b88899f02655ec2e
Dmitriy Snitko <sr480@live.ru>
2024-09-14 14:16:32 +0300

    Добавить уникальность логина и email, добавить валидацию пароля

--- SpecBox.WebApi/Model/Auth/UserRegisterModel.cs
+++ SpecBox.WebApi/Model/Auth/UserRegisterModel.cs
@@ -17,5 +17,8 @@ public class UserRegisterModel
   public string Name { get; set; } = null!;
 
   [Required]
+  [MinLength(8)]
+  [MaxLength(40)]
+  [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$")]
   public string Password { get; set; } = null!;
 }
